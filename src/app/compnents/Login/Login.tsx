047259aae1d1a227a1ac811f4d4744209914a01e
ehilel <ehilel@g.jct.ac.il>
2024-11-26 10:02:08 +0200

    Completion of the website registration process

--- src/app/compnents/Login/Login.tsx
+++ src/app/compnents/Login/Login.tsx
@@ -3,6 +3,9 @@ import React from "react";
 import { Box, Button, Typography, Card } from "@mui/material";
 import { styled } from "@mui/system";
 import { auth, provider, signInWithPopup } from "@/lib/firebase";
+import useUserStore from "@/services/zustand/userZustand/userStor";
+import { logInUser } from "@/services/userService";
+
 
 const IconContainer = styled(Box)({
   display: "flex",
@@ -27,29 +30,25 @@ const IconBase = styled(Box)({
   backgroundColor: "#a17f00",
 });
 const Login = () => {
+  const setUser = useUserStore((state) => state.setUser);
+
   const handleGoogleLogin = async () => {
     try {
       const result = await signInWithPopup(auth, provider);
       const user = result.user;
-      console.log("User info:", user);
-      // את יכולה לשמור את המשתמש ב-DB או לנתב לדף אחר
+      const localUser = {
+        _id: user.uid,
+        email: user.email || '',
+        name: user.displayName || '',
+        books: [],
+      }
+      setUser(localUser);
+      logInUser(localUser);
     } catch (error) {
       console.error("Error during Google login:", error);
     }
   };
 
-  // return (
-  //   <div style={styles.container}>
-  //     <div style={styles.card}>
-  //       <div style={styles.iconContainer}>
-  //         <div style={styles.iconCircle}></div>
-  //         <div style={styles.iconBase}></div>
-  //       </div>
-  //       <h2 style={styles.title}>בוא נחבר אותך:</h2>
-  //       <button style={styles.button} onClick={handleGoogleLogin}>המשך עם Google</button>
-  //     </div>
-  //   </div>
-  // );
   return (
     <Box
       sx={{
@@ -96,58 +95,6 @@ const Login = () => {
       </Card>
     </Box>
   );
-};
-
-const styles = {
-  container: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "100vh",
-    backgroundColor: "#333",
-  },
-  card: {
-    backgroundColor: "#222",
-    borderRadius: "16px",
-    padding: "20px",
-    textAlign: "center",
-    width: "300px",
-    boxShadow: "0 4px 10px rgba(0, 0, 0, 0.3)",
-  },
-  iconContainer: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    marginBottom: "20px",
-  },
-  iconCircle: {
-    width: "50px",
-    height: "50px",
-    borderRadius: "50%",
-    backgroundColor: "#d4a200",
-  },
-  iconBase: {
-    width: "80px",
-    height: "20px",
-    marginTop: "-10px",
-    borderRadius: "10px",
-    backgroundColor: "#a17f00",
-  },
-  title: {
-    color: "#fff",
-    fontSize: "18px",
-    margin: "10px 0",
-  },
-  button: {
-    backgroundColor: "#d4a200",
-    color: "#fff",
-    fontSize: "16px",
-    padding: "10px 20px",
-    border: "none",
-    borderRadius: "8px",
-    cursor: "pointer",
-    transition: "background-color 0.3s",
-  },
 };
 
 export default Login;
