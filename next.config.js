f4fdf7e481a40ed14dd5090ed21c24930c01cb7f
RacheliShayzada <054racheli@gmail.com>
2024-11-28 10:15:46 +0200

    adding filter component

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 const nextConfig = {
     reactStrictMode: true,
     env: {
-        PUBLIC_DB_CONNECTION: "mongodb+srv://ruthiedel866:bTo09ddiofqsWZHI@cluster0.ri87r.mongodb.net/Books?retryWrites=true&w=majority&appName=Cluster0",
      }};
   module.exports = nextConfig;
 
