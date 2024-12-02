26ac5bf97bcd1462ee535e0284fa571facb3cd49
RacheliShayzada <054racheli@gmail.com>
2024-12-02 14:08:23 +0200

    changing user dashboard style.

--- src/components/userStatus/userContainer.tsx
+++ src/components/userStatus/userContainer.tsx
@@ -5,7 +5,7 @@ import Recommendations from './Recommendations';
 
 export default function UserContainer() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 h-screen m-8">
+    <div className="grid grid-cols-1 md:grid-cols-2 m-8">
       <div className="">
         <div className="">
           <Information />
