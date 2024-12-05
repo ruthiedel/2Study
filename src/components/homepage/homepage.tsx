0cf6953d1c2f771b0605cfc70b2dfb0e1982d9a1
Esty Friedman <ehshpiner@gmail.com>
2024-12-05 10:38:24 +0200

    fix gematria

--- src/components/homepage/homepage.tsx
+++ src/components/homepage/homepage.tsx
@@ -1,36 +1,30 @@
 "use client";
-import React from "react";
 
+import React from "react";
 import styles from "./homepage.module.css";
+import useUserStore from '../../services/zustand/userZustand/userStor';
 
 
 const Homepage = () => {
-   
+    const user = useUserStore((state) => state.user);
+    const logout = useUserStore((state) => state.logout);
 
     return (
-
         <div className="min-h-screen bg-black text-white flex flex-col items-center justify-center">
-            {/* Header */}
             <header className="w-full bg-black py-4 shadow-lg">
                 <div className="container mx-auto px-6 flex justify-between items-center">
                     <h1 className="text-3xl font-bold text-gold">Study Progress Tracker</h1>
                     <nav>
-                        <a
-                            href="#"
-                            className="text-white hover:text-gold px-4 transition"
-                        >
+                        <a href="#"
+                            className="text-white hover:text-gold px-4 transition">
                             Home
                         </a>
-                        <a
-                            href="#about"
-                            className="text-white hover:text-gold px-4 transition"
-                        >
+                        <a href="#about"
+                            className="text-white hover:text-gold px-4 transition">
                             About
                         </a>
-                        <a
-                            href="#contact"
-                            className="text-white hover:text-gold px-4 transition"
-                        >
+                        <a href="#contact"
+                            className="text-white hover:text-gold px-4 transition">
                             Contact
                         </a>
                     </nav>
@@ -54,7 +48,7 @@ const Homepage = () => {
             <h2>לימוד הלכה ואמונה ב-2 דקות ביום</h2>
             <p>קטעי לימוד קצרים, מותאמים אישית לציבור החרדי, עם אפשרות לשמירת סימניות ומעקב אחר התקדמותך.</p> */}
             </div>
-
+        </div>
     );
 };
 
