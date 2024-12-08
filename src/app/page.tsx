e407e23c18a68df9225acd808fe3fbbb04e5969d
RacheliShayzada <054racheli@gmail.com>
2024-12-08 10:26:58 +0200

    adding wizard component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,19 @@
 
 import React from 'react';
 import { Homepage } from '@/components';
+import Wizard from '@/components/Graghs/Wizard/Wizard';
 
 
 const SomeComponent = () => {
+  const StepOne = () => <div>Step 1: Welcome to the Wizard! 🎉</div>;
+  const StepTwo = () => <div>Step 2: Let's add some details! ✍️</div>;
+  const StepThree = () => <div>Step 3: Review your progress! 📊</div>;
+  const steps = [<StepOne key="1" />, <StepTwo key="2" />, <StepThree key="3" />];
 
   return (
     <div>
       <Homepage/>
+      <Wizard steps={steps} />
     </div>
   );
 };
