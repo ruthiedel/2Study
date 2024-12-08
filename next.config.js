140dcaa35d8674033c8f080fbc7ec40002f5d3a1
Ruthi Edel <140281471+ruthiedel@users.noreply.github.com>
2024-12-08 11:53:45 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 
 const nextConfig = {
+const nextConfig = {
+  webpack(config) {
+    // דחוף את קבצי ה-CSS שלך אחרי כל השאר
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
 
 };
 
