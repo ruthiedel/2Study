d9e9e8a693356430b54585111d0d7a0c83e1f9ad
Esty Friedman <ehshpiner@gmail.com>
2024-12-11 09:51:49 +0200

    fix update mark index

--- src/components/MarkButton/MarkButton.tsx
+++ src/components/MarkButton/MarkButton.tsx
@@ -1,6 +1,5 @@
+import useUserStore from "@/services/zustand/userZustand/userStor";
 
-// import { updateBook } from '../../services/bookService'
-import { useUpdateBook } from '../../hooks/booksDetails';
 
 interface markProp {
     bookId: string;
@@ -8,19 +7,35 @@ interface markProp {
     paragraphId: number;}
 
 export default function MarkButton({ bookId, chapterId, paragraphId }: markProp) {
-
+    const user = useUserStore((state) => state.user);
+    const updateUserZustand = useUserStore((state) => state.updateUserZustand);
+  
+  
 
 function handleIndex(){
 
     try {
-        
-    } catch (e) {}
+        if (!user ||!Array.isArray(user.books)) return;
+        const bookIndex = user?.books.findIndex(book => book.book_id === bookId);
+        if (bookIndex !== -1) {
+            const updatedUser = {
+                ...user,
+                books: user.books.map((book, index) =>
+                  index === bookIndex ? { ...book, chapter_id: chapterId, section_id: paragraphId } : book
+                ),
+              };
+            updateUserZustand(user._id!, updatedUser); 
+        }
+    
+    } catch (e) {
+        console.log(e);
+    }
 
 }
 
 return (
     <div>
-      <button onClick={handleIndex}>עצרתי כאן</button>
+      <button onClick={handleIndex} className="mark-button">עצרתי כאן</button>
     </div>
  
 )
