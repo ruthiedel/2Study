41f9fab65c0c6573ea731e8e564aecb4f2dd71ec
Esty Friedman <ehshpiner@gmail.com>
2024-12-15 14:40:06 +0200

    adition text if no books in graphs and stadyComp

--- src/components/Graghs/BarGraph.tsx
+++ src/components/Graghs/BarGraph.tsx
@@ -15,7 +15,6 @@ import { calculateLearningProgress } from "../../lib/clientHelpers/graphsFunctio
 import { getBooks } from "@/hooks/booksDetails";
 import { Book } from "@/types";
 
-// Register the required components for Chart.js
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 const BarGraph = () => {
@@ -78,6 +77,7 @@ const BarGraph = () => {
 
   return (
     <div>
+      {(user && user.books && user.books.length === 0) && <p className='text-xs'>כאן יופיע פילוח הקטגוריות של הספרים בהם תלמד</p>}
       <Bar data={data} options={options} />
     </div>
   );
