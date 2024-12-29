04091caef5ecb92eb7e061d8b478bd43d8ec7890
Esty Friedman <ehshpiner@gmail.com>
2024-12-29 14:03:12 +0200

    move all sweet alert and zod scheema

--- src/lib/clientHelpers/zodSchema.ts
+++ src/lib/clientHelpers/zodSchema.ts
@@ -11,3 +11,22 @@ export const registerSchema = loginSchema.extend({
     errorMap: () => ({ message: "חובה לאשר את התקנון כדי להירשם" }),
   }),
 });
+
+export const contactSchema = z.object({
+    name: z.string().min(1, '*שדה חובה'),
+    email: z.string().nonempty('*שדה חובה').email('*כתובת מייל לא תקינה'),
+    message: z.string().min(1, '*שדה חובה'),
+});
+
+export const passwordSchema = z.object({
+    password: z
+        .string()
+        .min(6, { message: 'הסיסמא צריכה להכיל לפחות 6 תווים' })
+        .regex(/[a-z]/, { message: 'הסיסמא צריכה לכלול אותיות קטנות' })
+        .regex(/[A-Z]/, { message: 'הסיסמא צריכה לכלול אותיות גדולות' })
+        .regex(/[0-9]/, { message: 'הסיסמא צריכה לכלול ספרות' }),
+    confirmPassword: z.string().min(6, { message: 'הסיסמא צריכה להכיל לפחות 6 תווים' }),
+}).refine(data => data.password === data.confirmPassword, {
+    message: 'הסיסמאות לא תואמות',
+    path: ['confirmPassword'],
+});
