dd4cb4b239f1748b6f27b7e095184c6e2675adb9
RacheliShayzada <054racheli@gmail.com>
2024-12-30 12:18:20 +0200

    daily-study-style-and-learning-logic

--- src/components/DailyContentFolder/ExpandedCard.tsx
+++ src/components/DailyContentFolder/ExpandedCard.tsx
@@ -1,25 +1,38 @@
-import React from "react";
+// ExpandedCard.tsx
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 import styles from "./DailyContent.module.css";
+import { addLineBreaks } from '../../lib/clientHelpers/breakLines';
 
 interface ContentItem {
   title: string;
-  text: string;
-  location: string;  // הוספתי שדה location
+  text: string | string[] | string[][];
+  location: string;
 }
 
 const ExpandedCard: React.FC<{
   item: ContentItem;
   onClose: () => void;
 }> = ({ item, onClose }) => {
-  return (
-    <div className={styles.expandedCard}>
-      <button className={styles.closeButton} onClick={onClose}>
-        סגור
-      </button>
-      <h3>{item.title}</h3>
-      <p>{item.location}</p> 
-      <p dangerouslySetInnerHTML={{ __html: item.text }}></p>
-    </div>
+  return ReactDOM.createPortal(
+    <div className={styles.overlay} onClick={onClose}>
+      <div
+        className={styles.expandedCard}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <button className={styles.closeButton} onClick={onClose}>
+          X
+        </button>
+        <h3 className={styles.cardTitle}>{item.title}</h3>
+        <p className={styles.cardSubtitle}>{item.location}</p>
+        <div className={styles.cardTextContainer}>
+          <p className={styles.cardText} dangerouslySetInnerHTML={{
+            __html: addLineBreaks(item.text),
+          }}></p>
+        </div>
+      </div>
+    </div>,
+    document.body
   );
 };
 
