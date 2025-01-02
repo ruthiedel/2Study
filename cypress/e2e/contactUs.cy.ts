95378cfaad858bfd60c3ac2353c57d2395d2bb1a
Esty Friedman <ehshpiner@gmail.com>
2025-01-02 14:50:52 +0200

    remode // from testing

--- cypress/e2e/contactUs.cy.ts
+++ cypress/e2e/contactUs.cy.ts
@@ -4,22 +4,22 @@ describe('Footer Contact Form', () => {
       cy.visit('http://localhost:3000/home'); 
     });
   
-    // it('should submit the contact form successfully', () => {
-    //   const nameInput = cy.get(`[name="name"]`);
-    //   const emailInput = cy.get(`[name="email"]`);
-    //   const messageInput = cy.get(`[name="message"]`);
-    //   const submitButton = cy.get(`button[type="submit"]`);
+    it('should submit the contact form successfully', () => {
+      const nameInput = cy.get(`[name="name"]`);
+      const emailInput = cy.get(`[name="email"]`);
+      const messageInput = cy.get(`[name="message"]`);
+      const submitButton = cy.get(`button[type="submit"]`);
   
-    //   nameInput.type('Test User');
-    //   emailInput.type('test@example.com');
-    //   messageInput.type('This is a test message.');
-    //   submitButton.click();
+      nameInput.type('Test User');
+      emailInput.type('test@example.com');
+      messageInput.type('This is a test message.');
+      submitButton.click();
         
-    //   //check reset
-    //   cy.get('[name="name"]').should('have.value', '');
-    //   cy.get('[name="email"]').should('have.value', '');
-    //   cy.get('[name="message"]').should('have.value', '');
-    //   });
+      //check reset
+      cy.get('[name="name"]').should('have.value', '');
+      cy.get('[name="email"]').should('have.value', '');
+      cy.get('[name="message"]').should('have.value', '');
+      });
   
     it('empty fields', () => {
       cy.get(`button[type="submit"]`).click(); 
